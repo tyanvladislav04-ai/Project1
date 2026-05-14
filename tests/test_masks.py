d2f1c74bbd06215916ee1700453535a24be4ee29
Vladislav Tyan <tyan_is@mail.ru>
2026-05-14 23:32:38 +1200

    add tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -10,9 +10,25 @@ def test_get_mask_account(account, mask_account):
     assert get_mask_account(account) == mask_account
 
 
+@pytest.fixture
+def acc_fix():
+    return "73654108430135871234"
+
+def test_get_mask_account_1(acc_fix):
+    assert get_mask_account(acc_fix) == "**1234"
+
+
 @pytest.mark.parametrize(
     "card_number, mask_card_number",
     [("7000792289606361", "7000 79** **** 6361"), ("", " ** **** "), ("12345678901234567890", "1234 56** **** 7890")],
 )
 def test_get_mask_card_number(card_number, mask_card_number):
     assert get_mask_card_number(card_number) == mask_card_number
+
+
+@pytest.fixture
+def card_number_fix():
+    return "7000792289603921"
+
+def test_get_mask_card_number_1(card_number_fix):
+    assert get_mask_card_number(card_number_fix) == "7000 79** **** 3921"
